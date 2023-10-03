eed7c23a51450f05146252d6c7b47221e7defff3
Zackery He <zackeryhe03@gmail.com>
2023-10-03 14:07:10 -0700

    Initial Commit

---
+++ Canvas Events Display/Canvas_Events_DisplayApp.swift
@@ -0,0 +1,17 @@
+//
+//  Canvas_Events_DisplayApp.swift
+//  Canvas Events Display
+//
+//  Created by Zackery He on 10/3/23.
+//
+
+import SwiftUI
+
+@main
+struct Canvas_Events_DisplayApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
